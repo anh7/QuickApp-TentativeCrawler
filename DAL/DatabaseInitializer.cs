709379392dd2c14a675851aca5cea07692916a23
Trương Nguyễn Hoàng Anh <tnha@netcompany.com>
2022-02-19 17:05:51 +0700

    Updated intial credentials for convenient logins

--- DAL/DatabaseInitializer.cs
+++ DAL/DatabaseInitializer.cs
@@ -52,8 +52,8 @@ namespace DAL
                 await EnsureRoleAsync(adminRoleName, "Default administrator", ApplicationPermissions.GetAllPermissionValues());
                 await EnsureRoleAsync(userRoleName, "Default user", new string[] { });
 
-                await CreateUserAsync("admin", "tempP@ss123", "Inbuilt Administrator", "admin@ebenmonney.com", "+1 (123) 000-0000", new string[] { adminRoleName });
-                await CreateUserAsync("user", "tempP@ss123", "Inbuilt Standard User", "user@ebenmonney.com", "+1 (123) 000-0001", new string[] { userRoleName });
+                await CreateUserAsync("admin", "Abc123.", "Inbuilt Administrator", "hoanganh727018@gmail.com", "+1 (123) 000-0000", new string[] { adminRoleName });
+                await CreateUserAsync("user", "Abc123.", "Inbuilt Standard User", "hoang.anh727018@gmail.com", "+1 (123) 000-0001", new string[] { userRoleName });
 
                 _logger.LogInformation("Inbuilt account generation completed");
             }
