e309714c9cfbebe1bcf26b2beb0f8fc8e60efdd3
Trương Nguyễn Hoàng Anh <tnha@netcompany.com>
2022-02-21 18:29:50 +0700

    Added migration for Keyword entities

---
+++ QuickApp/Migrations/20220221112735_AddKeywordEntities.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace QuickApp.Migrations
+{
+    public partial class AddKeywordEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KeywordFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FileContent = table.Column<string>(type: "text", nullable: true),
+                    CreatedBy = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    UpdatedBy = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeywordFiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KeywordResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Keyword = table.Column<string>(type: "text", nullable: true),
+                    AdWordsAdverstiserCount = table.Column<int>(type: "integer", nullable: false),
+                    LinkCounts = table.Column<int>(type: "integer", nullable: false),
+                    TotalEstimatedCount = table.Column<long>(type: "bigint", nullable: false),
+                    TotalEstimatedTime = table.Column<double>(type: "double precision", nullable: false),
+                    FirstPageHTML = table.Column<string>(type: "text", nullable: true),
+                    KeywordFileId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedBy = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    UpdatedBy = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeywordResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_KeywordResults_KeywordFiles_KeywordFileId",
+                        column: x => x.KeywordFileId,
+                        principalTable: "KeywordFiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KeywordResults_KeywordFileId",
+                table: "KeywordResults",
+                column: "KeywordFileId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KeywordResults");
+
+            migrationBuilder.DropTable(
+                name: "KeywordFiles");
+        }
+    }
+}
