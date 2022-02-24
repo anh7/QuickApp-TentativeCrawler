553641f40385905f541afa4aef32aabfb3a930ed
Trương Nguyễn Hoàng Anh <tnha@netcompany.com>
2022-02-24 22:40:25 +0700

    Added controller for Keyword

---
+++ QuickApp/Controllers/KeywordController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DAL;
+using AutoMapper;
+
+namespace QuickApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class KeywordController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public KeywordController(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        // POST api/keyword
+        [HttpPost]
+        public IActionResult Post()
+        {
+            var file = Request.Form.Files[0];
+
+            return Ok();
+        }
+    }
+}
